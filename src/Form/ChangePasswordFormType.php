839594e1a625555dee5b80a99a3a47b753eb098d
martin claver yao <martinclaver2000@gmail.com>
2024-11-08 11:32:57 +0300

    ui reset-password

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -30,7 +30,7 @@ class ChangePasswordFormType extends AbstractType
                             'message' => 'Please enter a password',
                         ]),
                         new Length([
-                            'min' => 12,
+                            'min' => 6,
                             'minMessage' => 'Your password should be at least {{ limit }} characters',
                             // max length allowed by Symfony for security reasons
                             'max' => 4096,
